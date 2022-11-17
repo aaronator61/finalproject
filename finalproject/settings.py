35bffe3687394a94616cb002dfd30eedb4e8bc37
hwatso02 <hwatso02@gmail.com>
2022-11-17 12:00:04 -0700

    On branch Dev 	modified:   finalproject/__pycache__/__init__.cpython-310.pyc 	modified:   finalproject/__pycache__/settings.cpython-310.pyc 	modified:   finalproject/__pycache__/urls.cpython-310.pyc 	modified:   finalproject/settings.py 	modified:   pages/__pycache__/__init__.cpython-310.pyc 	modified:   pages/__pycache__/admin.cpython-310.pyc 	modified:   pages/__pycache__/apps.cpython-310.pyc 	modified:   pages/__pycache__/models.cpython-310.pyc 	modified:   pages/__pycache__/urls.cpython-310.pyc 	modified:   pages/__pycache__/views.cpython-310.pyc 	new file:   pages/migrations/0001_initial.py 	modified:   pages/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   pages/models.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'cruise_ratings',
+    'USER': 'postgres',
+    'PASSWORD': 'Ch3rryBl0$$0m',
+    'HOST': 'localhost'
     }
-}
+}  
 
 
 # Password validation
