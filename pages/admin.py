4c18a9f2b416226444491f6fd09deec716a2da17
hwatso02 <hwatso02@gmail.com>
2022-11-17 20:53:38 -0700

    upload admin code

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CruiseLine, Ship, Trip, User, Review
 
 # Register your models here.
+admin.site.register(CruiseLine)
+admin.site.register(Ship)
+admin.site.register(Trip)
+admin.site.register(User)
+admin.site.register(Review)
